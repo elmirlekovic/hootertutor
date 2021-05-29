121f71f20933735314ec8b450d579c1d897931b7
ayfor <josh.a.stubbington@gmail.com>
2021-05-29 12:56:25 -0400

    added reference columns to helprequest model

--- models/HelpRequest.js
+++ models/HelpRequest.js
@@ -16,6 +16,22 @@ HelpRequest.init(
             type: DataTypes.INTEGER,
             allowNull: false,
         },
+        //This column references the student that sent the help request
+        student_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model:'student',
+                key: 'id',
+            },
+        },
+        //This column references the id of the tutor that accepted the help request
+        tutor_id: {
+            type: DataTypes.INTEGER,
+            references:{
+                model: 'tutor',
+                key: 'id',
+            }
+        }
     },
     {
       sequelize,
