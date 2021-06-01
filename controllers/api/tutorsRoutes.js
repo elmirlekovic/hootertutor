6c7a69cffcd672524fb9363f4969cf26c43ea0f1
Elmir Lekovic <elmir123@gmai.com>
2021-06-01 19:40:32 -0400

    Teacher/User Flags

--- controllers/api/tutorsRoutes.js
+++ controllers/api/tutorsRoutes.js
@@ -1,10 +1,13 @@
 const router = require('express').Router();
 const { Tutor } = require('../../models');
 
-router.post('/tutor', async (req, res) => {
+router.get('/tutor/', async (req, res) => {
     try {
       const tutorData = await Tutor.create({
-        
+        user_id:req.session.user_id,
+        available_duration:req.body.available_duration,
+        is_available:req.body.is_available,
+        subject:req.body.subject
       });
       res.status(200).json(tutorData);
     } catch (err) {
@@ -12,5 +15,18 @@ router.post('/tutor', async (req, res) => {
       res.status(500).json(err);
     }
   });
-
+router.post('/tutor/', async (req, res) => {
+    try {
+        const tutorData = await Tutor.create({
+        user_id:req.session.user_id,
+        available_duration:req.body.available_duration,
+        is_available:req.body.is_available,
+        subject:req.body.subject
+    });
+        res.status(200).json(tutorData);
+    } catch (err) {
+        console.log(err);
+        res.status(500).json(err);
+    }
+    });
 module.exports = router;
