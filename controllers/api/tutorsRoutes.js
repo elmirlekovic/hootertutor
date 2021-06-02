b7ddee5919905ecaa3202cc0f96c9f802fec7436
Elmir Lekovic <elmir123@gmai.com>
2021-06-01 21:33:37 -0400

    HomeRoutes JS

--- controllers/api/tutorsRoutes.js
+++ controllers/api/tutorsRoutes.js
@@ -1,20 +1,7 @@
 const router = require('express').Router();
 const { Tutor } = require('../../models');
 
-router.get('/tutor/', async (req, res) => {
-    try {
-      const tutorData = await Tutor.create({
-        user_id:req.session.user_id,
-        available_duration:req.body.available_duration,
-        is_available:req.body.is_available,
-        subject:req.body.subject
-      });
-      res.status(200).json(tutorData);
-    } catch (err) {
-      console.log(err);
-      res.status(500).json(err);
-    }
-  });
+
 router.post('/tutor/', async (req, res) => {
     try {
         const tutorData = await Tutor.create({
