189497829d41658c4f6faeb9685833f67a1cca90
Elmir Lekovic <elmir123@gmai.com>
2021-06-01 21:34:17 -0400

    HomeRoutes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Tutor } = require('../models');
 
 // GET all galleries for homepage
 // Login route
@@ -7,14 +7,31 @@ router.get('/', (req, res) => {
     // If the user is already logged in, redirect to the homepage
     if (req.session.loggedIn) {
       if (req.session.is_teacher){
-        req.redirect('/tutor');
+        req.redirect('/tutor-portal');
       }
       if (req.session.is_student){
-        req.redirect('/student');
+        req.redirect('/student-portal');
       }
       return;
     }
     // Otherwise, render the 'login' template
     res.render('login');
+  });
+
+router.get('/tutor-portal', (req, res) => {
+    user = User.findByPk(req.session.user_id)
+    
+    if (req.session.is_student){
+        req.redirect('/student-portal');
+    }
+    res.render('tutor-portal', { user }); 
+  });
+
+router.get('/student-portal', (req, res) => {
+    user = User.findByPk(req.session.user_id)
+    if (req.session.is_teacher){
+        req.redirect('/tutor-portal');
+    }
+    res.render('student-portal', { user }); 
   });
 module.exports = router;
