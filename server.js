0decec8e47e80314d8c4cd760301f9da05c7b164
ayfor <josh.a.stubbington@gmail.com>
2021-06-03 18:57:28 -0400

    added handlebars to server

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
+ 
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -8,6 +11,7 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
 
 const sess = {
   secret: 'Super secret secret',
@@ -20,6 +24,10 @@ const sess = {
 };
 
 app.use(session(sess));
+
+// Inform Express.js on which template engine to use
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
