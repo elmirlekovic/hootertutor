03a0311e979bda36eab831ea8b77d3be207c3860
Elmir Lekovic <elmir123@gmai.com>
2021-06-05 08:42:17 -0400

    Tutor Page

--- public/js/tutor_help.js
+++ public/js/tutor_help.js
@@ -3,9 +3,11 @@ $( ".reqbutton" ).on( "click", function(event){
    $.get($(this).attr('href'), function (data, textStatus, jqXHR) {
     if(textStatus=='success'){
         $("#success").text(data.message).show().delay(3000).fadeOut(300);
-        $(this).text("Accepted Request");
+        let request_id = $(this).attr("data-rid");
+        $(this).hide();
+        $('#emailButton'+request_id).show();
         //$(this).removeClass();
-        $(this).removeAttr('href',"mailto:{{request.studentKey.user.email}}");
+        $(this).removeAttr('href',"mailt:{{request.studentKey.user.email}}");
     }else{
         $("#error").text(data.message).show().delay(3000).fadeOut(300);
     }
